6cc9569ff23575d92442bea35dfefe2958af23c7
polpuigdemont <polpuigdemont@gmail.com>
2023-10-21 17:02:06 +0200

    cleanup

--- maze_generator.py
+++ maze_generator.py
@@ -31,15 +31,10 @@ def generate_maze(n: int, m: int) -> List[List[str]]:
     maze.generate()
     maze.generate_entrances()
     strmaze = str(maze)
-    # replace " " with CHAR_IDS["open"]
     strmaze = strmaze.replace(" ", CHAR_IDS["open"])
-    # replace "#" with CHAR_IDS["wall"]
-    # count
     strmaze = strmaze.replace("#", CHAR_IDS["wall"])
-    # replace "E" with CHAR_IDS["door"]
     strmaze = strmaze.replace("E", CHAR_IDS["door"])
     strmaze = strmaze.replace("S", CHAR_IDS["wall"])
-    # get a list of all the coordinates in the grid that are not walls
     strmaze = string_to_matrix(strmaze)
     open_coords = [
         (x, y)
@@ -47,19 +42,15 @@ def generate_maze(n: int, m: int) -> List[List[str]]:
         for y in range(len(strmaze[0]))
         if strmaze[x][y] != CHAR_IDS["wall"]
     ]
-    # spawn monsters on open coordinates ranodomly
     for _ in range(ENEMY_COUNT):
         monster_x, monster_y = random.choice(open_coords)
         strmaze[monster_x][monster_y] = CHAR_IDS["enemy"]
         open_coords.remove((monster_x, monster_y))
-    # spawn key on open coordinates ranodomly
     for _ in range(KEY_COUNT):
         key_x, key_y = random.choice(open_coords)
         strmaze[key_x][key_y] = CHAR_IDS["key"]
         open_coords.remove((key_x, key_y))
-    # define a random starting point
     start_x, start_y = random.choice(open_coords)
-    # replace "S" with CHAR_IDS["entrance"]
     strmaze[start_x][start_y] = CHAR_IDS["entrance"]
     return ["".join(s) for s in strmaze]
 
