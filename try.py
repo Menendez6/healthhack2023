d9d8a44f175158863e0b75db39c030fc31b4a961
Menendez6 <pabloj.menendez@gmail.com>
2023-10-21 17:06:25 +0200

    adding sound

--- try.py
+++ try.py
@@ -6,15 +6,16 @@ import random
 import pygame
 import serial
 import time
+from tts import text_to_speech
 
 #We need to set a signal when the game starts and when the game finishes
-arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
+#arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
 
 def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
+    #arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
-    data = arduino.readline()
-    return data
+    #data = arduino.readline()
+    return "suuuu"
 
 # Class for the orange dude
 class Player(object):
@@ -56,7 +57,7 @@ class Player(object):
                     self.rect.top = wall.rect.bottom
                     value = write_read('1')
 
-        if self.rect.colliderect(door_rect) and not player.hasKey:
+        if self.rect.colliderect(door.rect) and not player.hasKey:
             if dx > 0: # Moving right; Hit the left side of the wall
                 self.rect.right = wall.rect.left
                 value = write_read('2')
@@ -93,7 +94,9 @@ class Player(object):
             dist_y_objective = str(abs(dist_y_objective)) + " cases en bas"
         else : 
             dist_y_objective = str(abs(dist_y_objective)) + " cases en haut"
+        message = "la " + objective + " est " + dist_x_objective + " et " + dist_y_objective 
         print("la", objective, "est", dist_x_objective, "et",dist_y_objective)
+        text_to_speech(message)
 
 
 class Key(object): 
