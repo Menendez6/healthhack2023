4458f2a921e9863e19074b6e69578f9fc54c356d
polpuigdemont <polpuigdemont@gmail.com>
2023-10-22 12:50:23 +0200

    arduino stuff to last version

--- vibration_sensor/vibrator.ino
+++ vibration_sensor/vibrator.ino
@@ -1,52 +1,56 @@
-int buzzerPin = 0;
-const unsigned long duration = 500;
-int key_received = 0;
+int buzzerPin = 0;  
+const unsigned long duration = 500; 
+int key_received=0;
 
-void setup()
-{
+void setup() {
+  //pinMode(buzzerPin, OUTPUT);  
   Serial.begin(115200);
   Serial.setTimeout(1);
+ 
 }
 
-void vibrate(int pin)
-{
+void vibrate(int pin){
   pinMode(pin, OUTPUT);
-  unsigned long startTime = millis(); // Get the current time
+  unsigned long startTime = millis();  // Get the current time
 
-  while (millis() - startTime < duration)
-  {
-
+  //Serial.print(x + 2);
+  while (millis() - startTime < duration) {
+    
     digitalWrite(pin, HIGH);
-    delay(50);
+    delay(50); 
 
+   
     digitalWrite(pin, LOW);
-    delay(50);
+    delay(50);  
   }
-
+  
+ 
   digitalWrite(pin, LOW);
+  
+  
 }
 
-void loop()
-{
-  while (!Serial.available())
-    ;
-  key_received = Serial.readString().toInt();
 
-  switch (key_received)
-  {
-  case 1: // up
-    buzzerPin = 9;
-    Serial.print(key_received + 1);
-    break;
-  case 2: // right
-    buzzerPin = 10;
-    break;
-  case 3: // down
-    buzzerPin = 11;
-    break;
-  case 4: // left
-    buzzerPin = 12;
-    break;
-  }
-  vibrate(buzzerPin);
+void loop() {
+  while (!Serial.available());
+  key_received = Serial.readString().toInt();
+  //if (Serial.available() >= 2) {
+  // received_key = Serial.read() + (Serial.read() << 8);
+  
+  switch (key_received){
+     case 1:
+       buzzerPin=9;
+       Serial.print(key_received + 1);
+       break;
+     case 2:
+       buzzerPin=10;
+       break;
+     case 3:
+       buzzerPin=11;
+       break;
+     case 4:
+       buzzerPin=12;
+       break;
+   }
+   vibrate(buzzerPin);
 }
